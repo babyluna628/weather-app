e62774b8fe8bcd4e418a17ad8a226a7a0ac1c2f9
dh <dohyun134@naver.com>
2025-01-14 16:07:10 +0900

    일단저장

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -4,7 +4,7 @@ import "./favorites.css";
 const Favorites = ({ favorites, onSelectFavorite }) => {
   return (
     <div className="favorites">
-      <h2>즐겨찾기</h2>
+      <h2>즐겨찾기 목록</h2>
       <ul>
         {favorites.map((city, index) => (
           <li key={index} onClick={() => onSelectFavorite(city)}>

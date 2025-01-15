242ef8ee9e282c40dc751d572a74d1b650eb3405
dh <dohyun134@naver.com>
2025-01-15 13:18:58 +0900

    검색어 추가완성

--- src/components/WeatherSearch.js
+++ src/components/WeatherSearch.js
@@ -7,7 +7,7 @@ const WeatherSearch = ({ onSearch }) => {
 
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places&language=en`;
     script.async = true;
     document.body.appendChild(script);
 

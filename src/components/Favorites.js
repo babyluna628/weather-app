f70d4f616c60d7c48df3ef2787e5ed2792913a48
dh <dohyun134@naver.com>
2025-01-15 10:45:59 +0900

    19버전으로 드래그완성

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import "./favorites.css";
 
 const Favorites = ({
